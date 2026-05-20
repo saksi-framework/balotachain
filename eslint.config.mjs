1f26c878a7ffbe1b75a00f380b542356bccc78d3
Banyel3 <138186128+Banyel3@users.noreply.github.com>
2026-05-21 00:54:56 +0800

    chore(ci): wire up multi-language CI pipeline (#2)

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    ignores: [
+      "node_modules/**",
+      "dist/**",
+      "target/**",
+      "coverage/**",
+      ".dart_tool/**",
+      "build/**",
+    ],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+];
